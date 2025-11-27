88d0a1a3a86c4b0918595d1d26a26d171008af96
nicoly <127155896+ottnicoly@users.noreply.github.com>
2025-11-27 02:49:44 +0000

    microkernel

--- microkernel-plugin/src/main/java/br/univille/microkernel_plugin/service/ServiceOneImpl.java
+++ microkernel-plugin/src/main/java/br/univille/microkernel_plugin/service/ServiceOneImpl.java
@@ -1,5 +1,20 @@
 package br.univille.microkernel_plugin.service;
 
-public class ServiceOneImpl {
+import java.util.HashMap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import br.univille.microkernel_interface.service.DefaultService;
+
+@Service
+public class ServiceOneImpl implements DefaultService{
+
+    @Override
+    public HttpStatus doWork(HashMap<String, Object> params) {
+        System.out.println("Service One" +params);
+        return HttpStatus.OK;
+    } 
+    
 
 }
